c689fee30faf850155f840290c0a393ee78a6ea2
NadavChen <nadav2282@gmail.com>
2024-04-24 13:22:49 +0300

    Added new class - CacheManager

--- test/CacheManager.java
+++ test/CacheManager.java
@@ -1,8 +1,35 @@
 package test;
 
+import java.util.HashSet;
+import java.util.Set;
 
 public class CacheManager {
-	
-	
+    private Set<String> cache;
+    private int maxSize;
+    private CacheReplacementPolicy replacementPolicy;
 
+    public CacheManager(int maxSize, CacheReplacementPolicy replacementPolicy) {
+        this.cache = new HashSet<>();
+        this.maxSize = maxSize;
+        this.replacementPolicy = replacementPolicy;
+    }
+
+    public boolean query(String word) {
+        return cache.contains(word);
+    }
+
+    public void add(String word) {
+        if (cache.size() >= maxSize) {
+            String evictWord = replacementPolicy.remove();
+            cache.remove(evictWord);
+        }
+        cache.add(word);
+        replacementPolicy.add(word);
+    }
+
+    public void remove(String word) {
+        if (cache.contains(word)) {
+            cache.remove(word);
+        }
+    }
 }
