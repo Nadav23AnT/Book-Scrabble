bdcdfff34c488d407bb80caecfc10b88ca2d3587
nadav chen <nadav2282@gmail.com>
2024-05-04 18:54:20 +0300

    init provided classes

---
+++ test/BookScrabbleHandler.java
@@ -0,0 +1,12 @@
+package test;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.io.IOException;
+
+public class BookScrabbleHandler implements ClientHandler {
+    
+}
---
+++ test/MyServer.java
@@ -0,0 +1,10 @@
+package test;
+
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.io.IOException;
+
+public class MyServer {
+    
+
+}
